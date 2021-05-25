b68fcd1bf6f5f94b2d957b4aea188fa3a10ab9ba
smallgram <30378053+smallgram@users.noreply.github.com>
2021-05-24 23:25:13 -0400

    feat(Tool): start Git log extraction process

--- src/patchnotes.nim
+++ src/patchnotes.nim
@@ -1,10 +1,24 @@
 import strformat
+import strutils
+import osproc
 
 import patchnotespkg/[common, message]
-
 
 
 when isMainModule:
   echo fmt"Patchnotes Tool v{patchnotesVersion}"
   echo fmt"Compiled at {CompileDate}: {CompileTime}"
   echo "------------------------------------"
+
+  # TODO: take in command line options:
+  #       path: path string, if blank, use CWD
+  #       start: commit or tag string, if blank, use earliest commit
+  #       end: commit or tag string, if blank, use most recent commit
+
+  let res = execCmdEx("git log --no-merges --abbrev-commit --format=medium",
+    {}, nil)
+
+  # TODO: parse git log here, passing it to a log parser which will
+  #       return nice data structures to work with, or right here...
+  for line in res.output.splitLines:
+    echo line

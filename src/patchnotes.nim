d660b315258a80b122538264a2773bc9599d73b2
smallgram <30378053+smallgram@users.noreply.github.com>
2021-05-25 23:04:45 -0400

    update todos

--- src/patchnotes.nim
+++ src/patchnotes.nim
@@ -10,16 +10,8 @@ when isMainModule:
   echo fmt"Patchnotes Tool v{patchnotesVersion}"
   echo fmt"Compiled at {CompileDate}: {CompileTime}"
   echo "------------------------------------"
-
-  # TODO: take in command line options:
-  #       path: path string, if blank, use CWD
-  #       start: commit or tag string, if blank, use earliest commit
-  #       end: commit or tag string, if blank, use most recent commit
-
+ 
   
-  
-  
-
   # TODO: parse git log here, passing it to a log parser which will
   #       return nice data structures to work with, or right here...
 
